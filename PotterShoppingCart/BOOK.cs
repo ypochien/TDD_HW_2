671700f7041c2727c2135738030873c861946003
Po Chien Yang <ypochien@gmail.com>
2015-10-26 15:58:24 +0800

    讓第6個測試案例通過。 //Scenario: 一二集各買了一本，第三集買了兩本，價格應為100*3*0.9 + 100 = 370

--- PotterShoppingCart/BOOK.cs
+++ PotterShoppingCart/BOOK.cs
@@ -4,5 +4,22 @@
     {
         public int Cost { get; set; }
         public string Name { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            BOOK b = (BOOK)obj;
+            if ((object)b == null) return false;
+            return b.Name == this.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+        }
     }
 }
